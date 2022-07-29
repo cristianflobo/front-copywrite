49c6460fb0f7df0e5f44c822c24be83697a37345
cristian lobo <cristianflobo@hotmail.com>
2022-07-29 00:59:13 -0500

    ok

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -13,12 +13,10 @@ const Home = () => {
   const onClick = async (e) => {
     const letra = e.target.value;
     try {
-      const result = await axios.get(
-        `/iecho?text=${letra}`
-      );
+      const result = await axios.get(`/iecho?text=${letra}`);
       let array = search.result;
       array.unshift(result.data.text);
-      console.log(array);
+
       setSearch({
         ...search,
         result: array,
@@ -28,41 +26,41 @@ const Home = () => {
     }
     //console.log(result)
   };
-  console.log(search.letra, search.result);
+
   return (
-    <div style={{backgroundColor:"lightgray", height:"100vh"}}>
-     
-        <nav
-          class="navbar navbar-dark bg-dark"
-          style={{ justifyContent: "center" }}
+    <div style={{ backgroundColor: "lightgray", height: "100vh" }}>
+      <nav
+        class="navbar navbar-dark bg-dark"
+        style={{ justifyContent: "center" }}
+      >
+        <form
+          class="form-inline"
+          style={{ display: "flex", flexDirection: "row", margin: "10px" }}
         >
-          <form
-            class="form-inline"
-            style={{ display: "flex", flexDirection: "row" }}
+          <input
+            style={{ width: "70vh" }}
+            class="form-control me-3"
+            type="search"
+            placeholder="insert text"
+            onChange={(e) => handleOnchange(e)}
+            aria-label="Search"
+          />
+          <button
+            onClick={(e) => onClick(e)}
+            value={search.letra}
+            type="button"
+            class="btn btn-primary"
           >
-            <input
-                style={{width:"70vh"}}
-              class="form-control me-3"
-              type="search"
-              placeholder="Search"
-              onChange={(e) => handleOnchange(e)}
-              aria-label="Search"
-            />
-            <button
-              onClick={(e) => onClick(e)}
-              value={search.letra}
-              type="button"
-              class="btn btn-primary"
-            >
-              Primary
-            </button>
-          </form>
-        </nav>
-      
-        <div style={{ justifyContent:"center", display:"flex" , marginTop:"40px"}}>
-            <Result text={search.result}></Result>
-        </div>
- 
+            Send
+          </button>
+        </form>
+      </nav>
+
+      <div
+        style={{ justifyContent: "center", display: "flex", marginTop: "40px" }}
+      >
+        <Result text={search.result}></Result>
+      </div>
     </div>
   );
 };
--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,14 +1,40 @@
 import React from "react";
-import "./style/Result.css"
+import "./style/Result.css";
 
 const Result = ({ text }) => {
   return (
-    <div className="result" style={{backgroundColor:"white", height:"70vh"}}>
-        <h1 >Result</h1>
-        <div >
-      {text.map((item) => (
-        <input value={item} style={{marginTop:"10px"}} type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1"/>
-      ))}
+    <div
+      className="result"
+      style={{ backgroundColor: "white", height: "70vh" }}
+    >
+      <h1 style={{marginLeft:"20px",marginTop:"40px"}}>Results:</h1>
+      <div
+        style={{
+          alignItems:"center",
+          display: "flex",
+          justifyContent: "center",
+          flexDirection: "column",
+        }}
+      >
+        {text.map((item) => (
+          <div style={{
+            
+            width: "60%",
+            display: "flex",
+            justifyContent: "center",
+            flexDirection: "column",
+          }}>
+          <input
+            value={item}
+            style={{ marginTop: "10px" }}
+            type="text"
+            class="form-control"
+            placeholder="Username"
+            aria-label="Username"
+            aria-describedby="basic-addon1"
+          />
+          </div>
+        ))}
       </div>
     </div>
   );
